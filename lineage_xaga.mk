c2f1255403b7ea98c9a1617e72a8cce709e43714
priiii1808 <priyanshusinghal0818@gmail.com>
2024-11-17 11:37:34 +0000

    xaga: Add our ROM flags
    Change-Id: Ifddfddee58d99e5232579e13aa063660c719f1be

--- lineage_xaga.mk
+++ lineage_xaga.mk
@@ -14,6 +14,15 @@ $(call inherit-product, device/xiaomi/xaga/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2460
+TARGET_SCREEN_WIDTH := 1080
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# ROM Flags
+TARGET_DISABLE_EPPE := true
+WITH_GMS := true
+
 PRODUCT_BRAND := POCO
 PRODUCT_DEVICE := xaga
 PRODUCT_MANUFACTURER := Xiaomi
