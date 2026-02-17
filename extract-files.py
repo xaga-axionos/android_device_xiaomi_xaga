89c2ffd1844496128c32f7d0527787cb4ddfc52d
ChrisCatto <chriscatto@proton.me>
2026-02-17 22:26:20 +0200

    xaga: Stop patching audio HW module against V33 libstagefright_foundation
    * Redundant after a recent change in common tree.
    
    Change-Id: Ic8a10ebace458b023c3daa8b47f7f369d50a5a5d
    Signed-off-by: ChrisCatto <chriscatto@proton.me>

--- extract-files.py
+++ extract-files.py
@@ -39,7 +39,6 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('(NXPLOG_.*_LOGLEVEL)=0x03', '\\1=0x02')
         .regex_replace('NFC_DEBUG_ENABLED=1', 'NFC_DEBUG_ENABLED=0'),
     'vendor/lib64/hw/audio.primary.mediatek.so': blob_fixup()
-        .add_needed('libstagefright_foundation-v33.so')
         .replace_needed('libalsautils.so', 'libalsautils-v31.so'),
     ('vendor/lib64/mt6895/libmtkcam_stdutils.so', 'vendor/lib64/hw/mt6895/android.hardware.camera.provider@2.6-impl-mediatek.so'): blob_fixup()
         .replace_needed('libutils.so', 'libutils-v32.so'),
