1e9ac25ee1a089269062159a93427a7027ab8fe3
bachnxuan <bachnxuan@gmail.com>
2026-02-18 12:44:11 +0700

    xaga: Debloat unwanted package
    Reference:
      - TelegramAt25/android_device_nothing_Aerodactyl
      - Jefino9488/android_device_xiaomi_xaga
    Signed-off-by: bachnxuan <bachnxuan@gmail.com>

---
+++ debloat/Android.mk
@@ -0,0 +1,17 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := debloat
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := \
+    Etar \
+    Glimpse \
+    Jelly \
+    Recorder \
+    Twelve \
+    Gallery2
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
--- device.mk
+++ device.mk
@@ -13,6 +13,10 @@ $(call inherit-product-if-exists, vendor/firmware/xaga/firmware.mk)
 # Call the MiuiCamera setup
 $(call inherit-product-if-exists, vendor/xiaomi/miuicamera-xaga/device.mk)
 
+# Debloat unwanted packages
+PRODUCT_PACKAGES += \
+    debloat
+
 # Fastboot package
 PRODUCT_BUILD_SUPER_PARTITION := true
 PRODUCT_FASTBOOT_TEMPLATE_ZIP := $(LOCAL_PATH)/prebuilts/fastboot.zip
